1aaf1131af928e31eb191366b795a5956ab0a43b
klaus <nickolai.beloglazov@gmail.com>
2021-12-04 21:58:57 +0600

    service-generator-api::optimize dependencies; change processing context model

--- src/main/java/com/home/servicegenerator/api/context/Context.java
+++ src/main/java/com/home/servicegenerator/api/context/Context.java
@@ -4,18 +4,16 @@ import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.Name;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
 public interface Context {
     Name getPipelineId();
     MethodDeclaration getPipeline();
-    Map<String, Object> getProperties();
-    Optional<Property> getPropertyByName(String name);
+    Optional<? extends Property> getPropertyByName(String name);
 
-    static Collection<? extends Property> requireNonNullValues(
-            final Collection<? extends Property> properties,
+    static Collection<Property> requireNonNullValues(
+            final Collection<Property> properties,
             final String errorMessage
     ) {
         if (properties.stream().map(Property::getValue).anyMatch(Objects::isNull)) {
